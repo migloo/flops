fe6849a6bdd22109c06c69b35787f7ec7f8dd98a
Clement Miglietti <migloo@gmail.com>
2016-05-31 21:52:35 +0200

    initial commit

---
+++ server-setup2.sh
@@ -0,0 +1,26 @@
+adduser deploy
+sudo adduser deploy sudo
+
+sudo apt-get update
+sudo apt-get install -y python python-pip python-virtualenv nginx supervisor
+
+cd /home/deploy
+virtualenv env
+source env/bin/activate
+
+pip install flask gunicorn
+mkdir flask_project && cd flask_project
+
+mkdir static
+nano app.py
+
+sudo rm /etc/nginx/sites-enabled/default
+sudo nano /etc/nginx/sites-available/flask_project
+
+sudo ln -s /etc/nginx/sites-available/flask_project /etc/nginx/sites-enabled/flask_project
+sudo nano /etc/supervisor/conf.d/flask_project.conf
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start flask_project
+sudo service nginx restart
